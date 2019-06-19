3a682a1b11cf8059ce440a15fa5670ec68512759
Yuji Mise <miseyu2000@gmail.com>
2019-06-19 10:37:44 +0900

    add: Pipfile and refactoring

--- src/notifier.py
+++ src/notifier.py
@@ -13,81 +13,83 @@ from message_builder import MessageBuilder
 # import re
 # import sys
 
-client = boto3.client('codepipeline')
-
-def findRevisionInfo(info):
-  r = client.get_pipeline_execution(
-    pipelineName=info.pipeline,
-    pipelineExecutionId=info.executionId
-  )['pipelineExecution']
-
-  revs = r.get('artifactRevisions',[])
-  if len(revs) > 0:
-      return revs[0]
-  return None
+client = boto3.client("codepipeline")
 
 
-def pipelineFromBuild(codeBuildInfo):
-  r = client.get_pipeline_state(name=codeBuildInfo.pipeline)
+def find_revision_info(info):
+    r = client.get_pipeline_execution(
+        pipelineName=info.pipeline, pipelineExecutionId=info.executionId
+    )["pipelineExecution"]
 
-  for s in r['stageStates']:
-    for a in s['actionStates']:
-      executionId = a.get('latestExecution', {}).get('externalExecutionId')
-      if executionId and codeBuildInfo.buildId.endswith(executionId):
-        pe = s['latestExecution']['pipelineExecutionId']
-        return (s['stageName'], pe, a)
-
-  return (None, None, None)
+    revs = r.get("artifactRevisions", [])
+    if len(revs) > 0:
+        return revs[0]
+    return None
 
 
-def processCodePipeline(event):
-  buildInfo = BuildInfo.fromEvent(event)
-  existing_msg = find_message_for_build(buildInfo)
-  builder = MessageBuilder(buildInfo, existing_msg)
-  builder.updatePipelineEvent(event)
+def pipeline_from_build(codeBuildInfo):
+    r = client.get_pipeline_state(name=codeBuildInfo.pipeline)
 
-  if builder.needsRevisionInfo():
-    revision = findRevisionInfo(buildInfo)
-    builder.attachRevisionInfo(revision)
-  
-  post_build_msg(builder)
+    for s in r["stageStates"]:
+        for a in s["actionStates"]:
+            executionId = a.get("latestExecution", {}).get("externalExecutionId")
+            if executionId and codeBuildInfo.buildId.endswith(executionId):
+                pe = s["latestExecution"]["pipelineExecutionId"]
+                return (s["stageName"], pe, a)
 
-def processCodeBuild(event):
-  cbi = CodeBuildInfo.fromEvent(event)
-  (stage, pid, actionStates) = pipelineFromBuild(cbi)
+    return (None, None, None)
 
-  if not pid:
-    return
 
-  buildInfo = BuildInfo(pid, cbi.pipeline)
+def process_code_pipeline(event):
+    buildInfo = BuildInfo.from_event(event)
+    existing_msg = find_message_for_build(buildInfo)
+    builder = MessageBuilder(buildInfo, existing_msg)
+    builder.update_pipeline_event(event)
 
-  existing_msg = find_message_for_build(buildInfo)
-  builder = MessageBuilder(buildInfo, existing_msg)
+    if builder.needs_revision_info():
+        revision = find_revision_info(buildInfo)
+        builder.attach_revision_info(revision)
 
-  if 'phases' in event['detail']['additional-information']:
-    phases = event['detail']['additional-information']['phases']
-    builder.updateBuildStageInfo(stage, phases, actionStates)
-  
-  logs = event['detail'].get('additional-information', {}).get('logs')
-  if logs:
-    builder.attachLogs(event['detail']['additional-information']['logs'])
-    
-  post_build_msg(builder)
+    post_build_msg(builder)
+
+
+def process_code_build(event):
+    cbi = CodeBuildInfo.from_event(event)
+    (stage, pid, actionStates) = pipeline_from_build(cbi)
+
+    if not pid:
+        return
+
+    buildInfo = BuildInfo(pid, cbi.pipeline)
+
+    existing_msg = find_message_for_build(buildInfo)
+    builder = MessageBuilder(buildInfo, existing_msg)
+
+    if "phases" in event["detail"]["additional-information"]:
+        phases = event["detail"]["additional-information"]["phases"]
+        builder.update_build_stage_info(stage, phases, actionStates)
+
+    logs = event["detail"].get("additional-information", {}).get("logs")
+    if logs:
+        builder.attach_logs(event["detail"]["additional-information"]["logs"])
+
+    post_build_msg(builder)
 
 
 def process(event):
-  if event['source'] == "aws.codepipeline":
-    processCodePipeline(event)
-  if event['source'] == "aws.codebuild":
-    processCodeBuild(event)
+    if event["source"] == "aws.codepipeline":
+        process_code_pipeline(event)
+    if event["source"] == "aws.codebuild":
+        process_code_build(event)
+
 
 def run(event, context):
-  #print(json.dumps(event, indent=2, default=str))
-  m = process(event)
+    m = process(event)
+
 
 if __name__ == "__main__":
-  with open ('test-event.json') as f:
-      events = json.load(f)
-      for e in events:
-        run(e, {})
-        time.sleep(1)
+    with open("test-event.json") as f:
+        events = json.load(f)
+        for e in events:
+            run(e, {})
+            time.sleep(1)
